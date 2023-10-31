299146e4e9592edf80a2c3cdd005cecb5b89ec18
clara <10088152+claraj@users.noreply.github.com>
2023-10-31 15:57:57 -0500

    remove body parser

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const bodyParser = require('body-parser')
 
 // import the routes in routes/index.js
 const indexRouter = require('./routes/index')
@@ -7,14 +6,11 @@ const indexRouter = require('./routes/index')
 // Create the web application
 const app = express() 
 
-// enable parsing of JSON data in a request body
-app.use(bodyParser.json())
-
 // make the routes in index.js available to the app 
 app.use('/', indexRouter)
 
 // Start server running 
-let server = app.listen(process.env.PORT || 3000, function() {
+let server = app.listen(process.env.PORT || 4000, function() {
     console.log('Express server running on port', server.address().port)
 })
 
